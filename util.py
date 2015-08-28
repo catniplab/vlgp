19200b9a550698652caf9628fb7e3384c60f7839
Yuan Zhao <yuan.zhao@stonybrook.edu>
2015-08-28 14:53:43 -0400

    incomplete cholesky decomposition

--- util.py
+++ util.py
@@ -22,3 +22,31 @@ def history(spike, p, intercept=True):
         else:
             regressor[t, intercept + (p - t) * N:] = spike[:t, :].flatten()
     return regressor
+
+
+def inchol(cov, tol=1e-5):
+    diag = np.ones(cov.shape[0])
+    pvec = np.arange(diag.size)
+    i = 0
+    g = np.zeros_like(cov)
+    while np.sum(diag[i:]) > tol:
+        if i > 0:
+            jast = np.argmax(diag[i:]) + i
+            pvec[i], pvec[jast] = pvec[jast], pvec[i]
+            g[i, :i + 1], g[jast, :i + 1] = g[jast, :i + 1], g[i, :i + 1]
+        else:
+            jast = 0
+
+        g[i, i] = np.sqrt(diag[jast])
+        if i < diag.size - 1:
+            newcol = cov[pvec[i + 1:], pvec[i]]
+            if i > 0:
+                g[i + 1:, i] = (newcol - np.dot(g[i + 1:, :i], g[i, :i].T)) / g[i, i]
+            else:
+                g[i + 1:, i] = newcol / g[i, i]
+
+        if i < diag.size - 1:
+            diag[i + 1:] = 1 - np.sum(np.square(g[i + 1:, :i]), axis=1)
+
+        i += 1
+    return g[:, :i], pvec[:i]
