45308ddcff0205284e66d05330bd33e16061dfcb
Yuan Zhao <yuan.zhao@stonybrook.edu>
2015-08-28 16:21:07 -0400

    incomplete cholesky decomposition

--- util.py
+++ util.py
@@ -33,13 +33,13 @@ def inchol(x, omega, tol=1e-5):
     while np.sum(diag[i:]) > tol:
         jast = np.argmax(diag[i:]) + i
         pvec[i], pvec[jast] = pvec[jast], pvec[i]
-        g[jast, :i + 1], g[i, :i + 1] = g[i, :i + 1].copy(), g[jast, :i + 1].copy()
+        g[jast, :i + 1][:], g[i, :i + 1][:] = g[i, :i + 1].copy(), g[jast, :i + 1].copy()
         g[i, i] = np.sqrt(diag[jast])
         g[i + 1:, i] = (np.exp(- omega * np.square(x[pvec[i + 1:]] - x[pvec[i]]))
                         - np.dot(g[i + 1:, :i], g[i, :i].T)) / g[i, i]
         diag[i + 1:] = 1 - np.sum(np.square(g[i + 1:, :i + 1]), axis=1)
 
         i += 1
-    return g, pvec, i - 1
+    return g[pvec, :i]
 
-g, pvec, i = inchol(np.arange(5), 1e-4)
+g = inchol(np.arange(5), 1e-4)
