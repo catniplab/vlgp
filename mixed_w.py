73bde16928113c736503c42177838ba5092d195d
Yuan Zhao <yuan.zhao@stonybrook.edu>
2015-11-04 15:20:29 -0500

    update w in compact form add docstring
    Former-commit-id: 2448a398171219e646f5e5c7bbf6f02dd4842af6

--- mixed_w.py
+++ mixed_w.py
@@ -11,20 +11,44 @@ UB = 20
 
 
 def vfromw(w, chol):
-    L, T, k = chol.shape
-    eyek = identity(k)
+    """Construct temporal slice of V from W
+    Args:
+        w: diagonals of W (T, L)
+        chol: cholesky factorizations of prior covariances (L, T, r)
+
+    Returns:
+        v: diagonals of V (T, L)
+    """
+    L, T, r = chol.shape
+    eyer = identity(r)
     v = empty((T, L), dtype=float)
     for l in range(L):
         G = chol[l, :]
         GTWG = G.T.dot(w[:, l].reshape((T, 1)) * G)
-        v[:, l] = sum(G * (G - G.dot(GTWG) + G.dot(GTWG.dot(linalg.solve(eyek + GTWG, GTWG, sym_pos=True)))),
-                         axis=1)
+        v[:, l] = sum(G * (G - G.dot(GTWG) + G.dot(GTWG.dot(linalg.solve(eyer + GTWG, GTWG, sym_pos=True)))),
+                      axis=1)
     return v
 
 
 def elbo(y, h, family, chol, m, w, v, a, b, vhat):
-    L, T, k = chol.shape
-    eyek = identity(k)
+    """Evidence Lower BOund
+    Args:
+        y: observations (T, N)
+        h: autocorrelation regressor (N, T, 1 + p)
+        family: distributions (N)
+        chol: cholesky factorizations of prior covariances (L, T, r)
+        m: posterior mean (T, L)
+        w: diagonals of W (T, L)
+        v: temporal slice of V (T, L)
+        a: latent coefficients (L, N)
+        b: autocorrelation coefficients (1 + p, N)
+        vhat: MLE of sample variances
+
+    Returns:
+
+    """
+    L, T, r = chol.shape
+    eyer = identity(r)
     poisson = family == 'poisson'
     gaussian = family == 'gaussian'
 
@@ -41,10 +65,10 @@ def elbo(y, h, family, chol, m, w, v, a, b, vhat):
     for l in range(L):
         G = chol[l, :]
         GTWG = G.T.dot(w[:, l].reshape((T, 1)) * G)
-        A = GTWG.dot(linalg.solve(eyek + GTWG, GTWG, sym_pos=True))
+        A = GTWG.dot(linalg.solve(eyer + GTWG, GTWG, sym_pos=True))
         m_div_G = linalg.lstsq(G, m[:, l])[0]
         tr = T - trace(GTWG) + trace(A)
-        lndet = np.linalg.slogdet(eyek - GTWG + A)[1]
+        lndet = np.linalg.slogdet(eyer - GTWG + A)[1]
 
         lb += -0.5 * inner(m_div_G, m_div_G) - 0.5 * tr + 0.5 * lndet
 
@@ -52,24 +76,46 @@ def elbo(y, h, family, chol, m, w, v, a, b, vhat):
 
 
 def accumulate(accu, grad, decay):
+    """adagrad
+    Args:
+        accu: accumulation matrix
+        grad: new gradient
+        decay: expoential decay
+
+    Returns:
+
+    """
     return decay * accu + (1 - decay) * grad ** 2
 
 
-def residual(y, h, family, chol, m, w, v, a, b, vhat):
-    poisson = family == 'poisson'
-    gaussian = family == 'gaussian'
-    res = empty_like(y, dtype=float)
-    eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
-    lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
-    res[:, poisson] = y[:, poisson] - lam[:, poisson]
-    res[:, gaussian] = (y[:, gaussian] - eta[:, gaussian]) / vhat[gaussian]
-    return res
+def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-5, decay=0.95, eps=1e-6, verbose=True):
+    """Variational Bayesian
+    Args:
+        y: observations (T, N), spikes or continuous
+        family: distributions (N), 'poisson' and 'gaussian' supported now
+        p: order of autocorrelation
+        chol: cholesky factorizations of prior covariances (L, T, r)
+        m0: initial posterior mean (T, L)
+        a0: initial latent coefficients (L, N)
+        b0: initial autocorrelation coefficients (1 + p, N)
+        niter: max number of iterations
+        tol: relative tolerance of convergence
+        decay: adagrad
+        eps: adagrad
+        verbose: detailed output
 
-
-def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, decay=0.9, eps=1e-6, verbose=True):
-    L, T, cholrank = chol.shape
+    Returns:
+        lbound: ELBO of all iterations
+        m: posterior mean (T, L)
+        lv: L matrices in factorization of V = LL' (L, T, r)
+        a: latent coefficients (L, N)
+        b: autocorrelation coefficients (1 + p, N)
+        elapsed: running time
+        converged: whether the algorithm converged within iteration limit
+    """
+    L, T, r = chol.shape
     N = y.shape[1]
-    eyek = identity(cholrank)
+    eyek = identity(r)
 
     poisson = family == 'poisson'
     gaussian = family == 'gaussian'
@@ -93,22 +139,21 @@ def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, dec
     b = b0
     m = m0
 
-    w = zeros((T, L), dtype=float)
+    U = empty((T, N), dtype=float)
+    R = empty_like(y, dtype=float)
+
     eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
-    lam = exp(eta.clip(LB, UB))
     vhat = var(y - eta, axis=0, ddof=0)
-    for l in range(L):
-        U = empty((T, N), dtype=float)
-        U[:, poisson] = lam[:, poisson]
-        U[:, gaussian] = 1 / vhat[gaussian]
-        w[:, l] = U.dot(a[l, :] ** 2)
+    lam = exp(eta.clip(LB, UB))  # no a'Va at beginning
+    U[:, poisson] = lam[:, poisson]
+    U[:, gaussian] = 1 / vhat[gaussian]
+    w = U.dot(a.T ** 2)
     v = vfromw(w, chol)
 
     # backup, could be useless
     good_m = m.copy()
     good_a = a.copy()
     good_b = b.copy()
-    good_w = w.copy()
     #
 
     lbound = full(niter, fill_value=finfo(float).min, dtype=float)
@@ -118,7 +163,6 @@ def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, dec
     accu_grad_a = zeros_like(a)
     accu_grad_b = zeros_like(b)
     accu_grad_m = zeros_like(m)
-    accu_delta_w = zeros_like(w)
 
     # dec = False
     converged = False
@@ -130,11 +174,11 @@ def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, dec
             print('\nIteration[{:d}]'.format(i))
 
         # estimate latent
-        eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
-        lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
-        vhat = var(y - eta, axis=0, ddof=0)
         for l in range(L):
             # m
+            eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
+            vhat = var(y - eta, axis=0, ddof=0)
+            lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
             G = chol[l]
             grad_m = (y[:, poisson] - lam[:, poisson]).dot(a[l, poisson]) + \
                      ((y[:, gaussian] - eta[:, gaussian]) / vhat[gaussian]).dot(a[l, gaussian]) \
@@ -145,7 +189,12 @@ def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, dec
             wada = (w[:, l] + sqrt(eps + accu_grad_m[:, l])).reshape((T, 1))  # adjusted by adagrad
             GTWG = G.T.dot(wada * G)
 
-            R = residual(y, h, family, chol, m, w, v, a, b, vhat)
+            # eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
+            # lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
+            R[:, poisson] = y[:, poisson] - lam[:, poisson]
+            R[:, gaussian] = (y[:, gaussian] - eta[:, gaussian]) / vhat[gaussian]
+
+            # R = residual(y, h, family, chol, m, w, v, a, b, vhat)
 
             u = G.dot(G.T.dot(R.dot(a[l, :]))) - m[:, l]
             delta_m = u - G.dot((wada * G).T.dot(u)) + \
@@ -158,9 +207,9 @@ def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, dec
             m[:, l] /= scale
 
         # estimate coefficients
-        eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
-        lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
         for n in range(N):
+            eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
+            lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
             if family[n] == 'poisson':
                 # a
                 va = v * a[:, n]  # (T, L)
@@ -192,19 +241,11 @@ def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, dec
         # update w
         eta = einsum('ijk, ki->ji', h, b) + m.dot(a)
         vhat = var(y - eta, axis=0, ddof=0)
-        for _ in range(10):
-            lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
-            for l in range(L):
-                U = empty((T, N), dtype=float)
-                U[:, poisson] = lam[:, poisson]
-                U[:, gaussian] = 1 / vhat[gaussian]
-                # w[:, l] = U.dot(a[l, :] ** 2)
-                d = U.dot(a[l, :] ** 2) - w[:, l]
-                # accu_delta_w[:, l] = accumulate(accu_delta_w[:, l], d, decay)
-                w[:, l] += d
-            print(linalg.norm(w - good_w) / linalg.norm(good_w))
-            good_w[:] = w
-            v = vfromw(w, chol)
+        lam = exp((eta + 0.5 * v.dot(a ** 2)).clip(LB, UB))
+        U[:, poisson] = lam[:, poisson]
+        U[:, gaussian] = 1 / vhat[gaussian]
+        w = U.dot(a.T ** 2)
+        v = vfromw(w, chol)
 
         lbound[i] = elbo(y, h, family, chol, m, w, v, a, b, vhat)
 
@@ -224,16 +265,18 @@ def train(y, family, p, chol, m0=None, a0=None, b0=None, niter=50, tol=1e-4, dec
         if abs(lbound[i] - lbound[i - 1]) < tol * abs(lbound[i - 1]):
             converged = True
 
+        # keep a copy of current iteration
+        # no use in algorithm
+        # for debug only now
         good_m[:] = m
         good_a[:] = a
         good_b[:] = b
-        good_w[:] = w
 
         i += 1
 
     stop = timeit.default_timer()
 
-    lv = empty((L, T, cholrank), dtype=float)
+    lv = empty((L, T, r), dtype=float)  # V = LL'
     for l in range(L):
         G = chol[l, :]
         GTWG = G.T.dot(w[:, l].reshape((T, 1)) * G)
