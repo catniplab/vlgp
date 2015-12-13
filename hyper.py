4e9f3ff9a05685ffd305bed45a27693fa5497788
yuan <yuan.zhao@stonybrook.edu>
2015-12-13 16:45:57 -0500

    no message
    Former-commit-id: 92f3b28b16605d1f627b1cfac74f5aab11a2e0df

--- hyper.py
+++ hyper.py
@@ -1,7 +1,8 @@
-from numpy import identity, inner, meshgrid, arange, outer, zeros_like, trace, empty_like, empty, sum
+from numpy import identity, inner, meshgrid, arange, outer, zeros_like, trace, empty_like, empty, sum, dstack, diag
 from numpy.core.umath import exp, log
 from numpy.linalg import slogdet
-from scipy.linalg import lstsq, solve
+from numpy.random.mtrand import choice
+from scipy.linalg import lstsq, solve, toeplitz, solve_toeplitz
 from scipy.optimize import minimize, minimize_scalar
 from mathf import ichol_gauss
 from util import sqexpcov
@@ -58,12 +59,6 @@ def klprime(theta, sigma, mu, L):
     return df
 
 
-def Kprime(sigma, theta, n):
-    r, c = meshgrid(arange(n), arange(n))
-    difsq = (r - c) ** 2
-    return -sigma ** 2 * exp(-exp(theta) * difsq) * exp(theta) * difsq
-
-
 def learn_hyper(model):
     theta = empty_like(model['omega'])
     nlatent = theta.shape[0]
@@ -71,3 +66,59 @@ def learn_hyper(model):
         theta[ilatent] = minimize(kl, x0=theta[ilatent], method='CG', args=(
         model['sigma'][ilatent], model['mu'][:, :, ilatent], model['L'][:, ilatent, :])).x
     return exp(theta)
+
+
+def Kprime(theta, sigma, n):
+    difsq = toeplitz(arange(n) ** 2)
+    omega = exp(theta)
+    sigmasq = sigma ** 2
+    return -sigmasq * exp(-omega * difsq) * omega * difsq
+
+
+def KLprime(theta, sigma, n, M, S, eps=1e-6):
+    omega = exp(theta)
+    sigmasq = sigma ** 2
+    nseg = M.shape[-1]
+    dsq = arange(n) ** 2
+    Dsq = toeplitz(dsq)
+    k = sigmasq * exp(-omega * dsq)
+    K = toeplitz(k)
+    KinvKD = solve_toeplitz(k, K * Dsq * omega)
+    grad = 0.0
+    for iseg in range(nseg):
+        KinvS = solve_toeplitz(k, S[:, :, iseg])
+        KinvM = solve_toeplitz(k, M[:, :, iseg])
+        grad += trace(solve(KinvM + KinvS - identity(n), KinvKD, sym_pos=True))
+    return grad / 2
+
+
+def KL(theta, sigma, n, mu, S, eps=1e-6):
+    omega = exp(theta)
+    sigmasq = sigma ** 2
+    nseg = mu.shape[-1]
+    dsq = arange(n) ** 2
+    k = sigmasq * exp(-omega * dsq)
+    div = 0.0
+    for iseg in range(nseg):
+        KinvS = solve_toeplitz(k, S[:, :, iseg])
+        muKinvmu = mu[:, iseg].dot(solve_toeplitz(k, mu[:, iseg]))
+        div += muKinvmu + trace(KinvS) - slogdet(KinvS)[1]
+    return div / 2
+
+
+def learngp(obj, window=50, nseg=10, eps=1e-6):
+    ntrial, ntime, nlatent = obj['mu'].shape
+    mu = obj['mu'].reshape((-1, nlatent))
+    w = obj['w'].reshape((-1, nlatent))
+    sigma = obj['sigma']
+    omega = obj['omega']
+    n = ntrial * ntime - window
+    start = choice(arange(n), size=nseg)
+    win_mu = dstack([mu[i:i + window, :] for i in start])
+    win_w = dstack([w[i:i + window, :] for i in start])
+    dsq = arange(window) ** 2
+    for ilatent in range(nlatent):
+        K = sigma[ilatent] ** 2 * exp(-omega[ilatent] * dsq)
+        S = dstack([K - K.dot(solve(diag(1 / (eps + win_w[:, ilatent, iseg])) + K, K, sym_pos=True)) for iseg in range(nseg)])
+        omega[ilatent] = minimize(KL, x0=log(omega[ilatent]), jac=KLprime, args=(sigma[ilatent], window, win_mu[:, ilatent, :], S), method='CG').x
+    return omega
