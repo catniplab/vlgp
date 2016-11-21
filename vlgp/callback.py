a66500921ebc7b0810408ac67159c5f247bd1e2e
yuan <yuan.zhao@stonybrook.edu>
2016-11-21 14:41:35 -0500

    import tqdm without enforcing it as a dependency

--- vlgp/callback.py
+++ vlgp/callback.py
@@ -1,7 +1,14 @@
 import time
 from pprint import pprint
 
-from tqdm import tqdm
+# import tqdm without enforcing it as a dependency
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(*args, **kwargs):
+        if args:
+            return args[0]
+        return kwargs.get('iterable', None)
 
 from .util import save
 
