ae3acf2b0e5d824fbe359c85456303603fdf238c
yuan <yuan.zhao@stonybrook.edu>
2016-12-22 17:05:00 -0500

    added slice sampler

--- vlgp/gp.py
+++ vlgp/gp.py
@@ -10,6 +10,8 @@ from scipy.linalg import cholesky, LinAlgError, cho_solve
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.spatial.distance import pdist, squareform
 
+from .math import ichol_gauss
+
 
 logger = logging.getLogger(__name__)
 
@@ -143,3 +145,119 @@ def subsample(n, size, successive=False):
     if successive:
         return np.arange(size) + np.random.randint(n - size)
     return np.random.choice(n, size, replace=False)
+
+
+def slice_sample(f, theta, scale, aux_std, rank):
+    """Slice sampling
+        Murray & Adams 2010 Algorithm 4
+    Parameters
+    ----------
+    f : ndarray
+        latent variable
+    theta : float
+        hyperparameter
+    aux_std : float
+        standard deviation of surrogate variable
+    rank : int
+        rank of incomplete Cholesky
+    """
+    # 1. Draw surrogate data: g ~ N(f, aux_std)
+    g = f + np.random.randn(f.size) * aux_std
+
+    # 2. Compute implied latent variates
+    L = 0.0
+    # eta = cho_solve((L, True), f - m)
+
+    # 3. Randomly cener a bracket
+    v = np.random.random() * scale
+    theta_min = theta - v
+    theta_max = theta_min + scale
+
+    # 4. Draw u ~ Uniform(0, 1)
+    u = np.random.random()
+
+    # 5. Determine threashold
+    # y = u * lik(f) * N(g; 0, prior cov + I* aux_std) * prior(theta)
+
+    # while true:
+    # 6. Draw proposal: theta' ~ Uniform(theta_min, theta_max)
+
+    # 7. Compute function f' = L'eta + m'
+
+    # if lik(f') N(g; 0, prior cov' + S') prior(theta') > y:
+    #    return f', theta'
+    # else if theta' < theta:
+    #    theta_min = theta'
+    # else
+    #    theta_max = theta'
+
+def metro_hast(theta, f, slice_width, n, rank):
+    log_prior_theta = lambda x: 0 if 1e-6 < x < 10 else -np.inf
+
+    params = {}
+    step_out = slice_width > 0
+    slice_width = abs(slice_width)
+    # slice_fn = lambda
+    params = slice_sweep(params, slice_fn, slice_width, step_out)
+    return params['position'], params['ichol']
+
+
+def update_params(params, lpstar_min, log_prior_theta, loglik, n, rank):
+    theta = params['position']
+    l_prior_theta = log_prior_theta(theta)
+
+    if not np.isfinite(l_prior_theta):
+        params['log_p_star'] = -np.inf
+        params['on_slice'] = False
+        return params
+
+    G = ichol_gauss(n, theta, rank)
+
+    q = cho_solve((G, True), params['f'])
+    log_prior_factor = -0.5 * q.T @ q - n * np.log(params['sigma'])
+    params['lpstar'] = log_prior_factor + log_prior_theta + loglik(params['f'])
+    params['on_slice'] = params['lpstar'] >= lpstar_min
+    params['ichol'] = G
+
+
+def slice_sweep(particle, slice_fn, slice_width=1, step_out=True):
+    dd = particle['position'].size
+    if slice_width.size == 1:
+        slice_width *= np.ones(dd)
+
+    for d in np.random.permutation(dd):
+        lpstar_min = particle['lpstar'] + np.log(np.random.random())
+        x_cur = particle['position']
+
+        rr = np.random.random()
+        x_l = x_cur - rr * slice_width[d]
+        x_r = x_cur + (1 - rr) * slice_width[d]
+
+        if step_out:
+            particle['position'][d] = x_l
+            while True:
+                particle = slice_fn(particle, lpstar_min)
+                if not particle['on_slice']:
+                    break
+                particle['position'][d] -= slice_width[d]
+            x_l = particle['position'][d]
+            particle['position'][d] = x_r
+            while True:
+                particle = slice_fn(particle, lpstar_min)
+                if not particle['on_slice']:
+                    break
+                particle['position'][d] += slice_width[d]
+            x_r = particle['position'][d]
+
+        chk = False
+        while True:
+            particle['position'][d] = np.random.random() * (x_r - x_l) + x_l
+            particle = slice_fn(particle, lpstar_min)
+            if particle['on_slice']:
+                break
+            if particle['position'][d] > x_cur:
+                x_r = particle['position'][d]
+            elif particle['position'][d] < x_cur:
+                x_l = particle['position'][d]
+            else:
+                raise ValueError('BUG DETECTED: Shrunk to current position and still not acceptable.')
