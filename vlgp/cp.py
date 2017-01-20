91d78e4097ce56ea6a2651add26906fee2d6cf54
yuan <yuan.zhao@stonybrook.edu>
2017-01-20 15:53:59 -0500

    add haefner's CP and weights function

--- vlgp/cp.py
+++ vlgp/cp.py
@@ -43,3 +43,57 @@ def spontaneous(loading, size):
     mu = loading @ vt.T @ np.random.randn(loading.shape[1], size) + 10
     # mu -= np.min(mu)
     return np.random.poisson(mu).T
+
+
+def haefner_cp(cov, weights, var=None, finite=True):
+    """computes choice probabilities CP implied by cov and weights
+        % if FINITE='finite' (default) a finite population is assumed
+        %
+        % if FINITE='infinite' an infinitely large population is assumed where
+        % cov and weights contain the discretization of the covariance
+        % function and the weights function
+        %
+        % currently works only for 2D C and 1D weights but can be generalized to
+        % incorporate time or, in the case of infinitely large populations,
+        % multiple dependencies
+    """
+
+    var = np.diag(cov) if finite else var
+
+    xsi = (cov @ weights) / np.sqrt(var * (weights.T @ cov @ weights))
+    cp = 0.5 + 2 / np.pi * np.sign(xsi) * np.arctan(1. / np.sqrt(2. / xsi ** 2 - 1))  # equation (1) in paper
+    return cp
+
+
+def haefner_weights(corr, cp, use):
+    """
+        % function [weights V lambda cp_model] = Weights(corr,cp,use)
+        %
+        % Weight reconstruction from input corr and CPs for a large
+        % neuronal population (limit of infinitely many neurons)
+        %
+        % INPUTS
+        % corr: correlation matrix
+        % cp: observed choice probabilities
+        % use: set of indices of eigenfunctions that are to be used in the
+        %      reconstruction (in descending order, i.e. use=[1 2] implies that
+        %      only the eigenfunctions with the largest and second-largest
+        %      eigenvalue are to be used
+        %
+        % OUTPUTS
+        % weights: implied weights for a large neuronal population
+        % V: set of eigenfunctions used for the weight reconstruction
+        % lambda : eigenvalues of the eigenfunctions used for weight reconstruction
+        % cp_model: choice probabilities implied by the reconstructed weights
+
+    """
+    from scipy.linalg import eigh
+
+    w, v = eigh(corr)
+    w = w[::-1]
+    v = v[:, ::-1]
+
+    nu = 1. / w[use] * (v[:, use] @ (cp - 0.5))
+    weights = v[:, use] @ nu
+
+    return weights, v, w, haefner_cp(corr, weights, np.ones(len(cp)), finite=False)
