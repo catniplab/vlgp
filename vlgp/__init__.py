fb2a7c742b79525ed40b7695640274918200a3ea
yuan <yuanz271@gmail.com>
2017-10-27 13:45:41 -0400

    use stream log handler

--- vlgp/__init__.py
+++ vlgp/__init__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 # logger.setLevel(logging.INFO)
 
 # # create a logging format
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # # create a file file_handler
 # # file_handler = logging.FileHandler('vlgp.log')
@@ -17,9 +17,9 @@ logger = logging.getLogger(__name__)
 # handler.setFormatter(formatter)
 # logger.addHandler(handler)
 
-# stdout_handler = logging.StreamHandler(sys.stdout)
-# stdout_handler.setLevel(logging.INFO)
-# logger.addHandler(stdout_handler)
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.INFO)
+logger.addHandler(stdout_handler)
 
 if sys.version_info[0] < 3 or sys.version_info[1] < 5:
     logger.warning(str(sys.version_info))
