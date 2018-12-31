4adb7195d3c32e6e7152f2b3bdb559fe327cb562
yuan <yuanz271@gmail.com>
2018-12-31 10:40:48 -0500

    Move VLGP to core

--- vlgp/base.py
+++ vlgp/base.py
@@ -16,24 +16,3 @@ class Model(metaclass=ABCMeta):
     @abstractmethod
     def load(self):
         pass
-
-
-class VLGP(Model):
-    def __init__(self, n_factors, random_state=0, **kwargs):
-        self.n_factors = n_factors
-
-    def fit(self, trials):
-        """Fit the vLGP model to data using vEM
-        :param trials: list of trials
-        :return: the trials containing the latent factors
-        """
-        return trials
-
-    def infer(self, trials):
-        raise NotImplementedError()
-
-    def save(self):
-        raise NotImplementedError()
-
-    def load(self):
-        raise NotImplementedError()
