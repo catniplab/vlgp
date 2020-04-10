ad04bccaeece49018dadedfccba0988c0f898073
yuan <yuanz271@gmail.com>
2020-04-10 14:56:42 -0400

    add the function doing VI based on MAP estimate

--- vlgp/api.py
+++ vlgp/api.py
@@ -3,6 +3,7 @@ import logging
 
 import click
 
+from . import gmap
 from .preprocess import get_params, get_config, fill_trials, fill_params, initialize
 from .callback import Saver, show
 from .core import vem, update_w, update_v, infer
@@ -73,3 +74,48 @@ def fit(trials, n_factors, **kwargs):
     result = {"trials": trials, "params": params, "config": config}
 
     return result
+
+
+def map2vi(trials, C, d, **kwargs):
+    import numpy as np
+    n_factors = trials[0]['mu'].shape[-1]
+    config = get_config(**kwargs)
+    logger.info("\n".join(["{} : {}".format(k, v) for k, v in config.items()]))
+
+    # add built-in callbacks
+    callbacks = config["callbacks"]
+    if "path" in config:
+        saver = Saver()
+        callbacks.extend([show, saver.save])
+    config["callbacks"] = callbacks
+
+    # prepare parameters
+    kwargs["omega_bound"] = config["omega_bound"]
+    params = get_params(trials, n_factors, **kwargs)
+
+    params['a'] = C
+    params['b'] = np.log(d)
+
+    make_cholesky(trials, params, config)
+    update_w(trials, params, config)
+    update_v(trials, params, config)
+
+    click.echo("Inferring")
+    infer(trials, params, config)
+    click.secho("Done", fg="green")
+
+    click.echo("Estimating parameters")
+    Eniter = config['Eniter']
+    config['Eniter'] = 0
+    infer(trials, params, config)
+    config['Eniter'] = Eniter
+    click.secho("Done", fg="green")
+
+    click.echo("Inferring")
+    infer(trials, params, config)
+    click.secho("Done", fg="green")
+
+    result = {"trials": trials, "params": params, "config": config}
+
+    return result
+
