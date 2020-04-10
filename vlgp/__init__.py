2eb1a9dce56aedbbd926723b8006b40ce964f362
yuan <yuanz271@gmail.com>
2020-04-10 14:51:56 -0400

    change log to file

--- vlgp/__init__.py
+++ vlgp/__init__.py
@@ -5,7 +5,10 @@ import logging
 import warnings
 
 logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    filename='vlgp.log',
+    filemode='a',
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
